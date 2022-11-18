f8927f3dc230e78e3b57313b3e8cd4b971eb99b0
Olga Bel <olza91@mail.ru>
2022-11-18 21:04:28 +0300

    dz9

---
+++ 001/Program.cs
@@ -0,0 +1,38 @@
+﻿/*Задача 64: Задайте значение N. Напишите программу, 
+которая выведет все чётные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+N = 5 -> "4, 2"
+N = 8 -> "8, 6, 4, 2,"*/
+
+Console.WriteLine("Введите число  ");
+int n = int.Parse(Console.ReadLine());
+
+EvenNumber(n);
+Console.WriteLine("проверка");
+Console.WriteLine(EvenNumberRec(n));
+
+void EvenNumber(int n)
+{
+    int number = 1;
+    while (n > number)
+    {
+        number++;
+        if ((number % 2) == 0)
+        {
+            Console.Write(number + ", ");
+        }
+    }
+}
+
+int EvenNumberRec(int n)  //рекурсия
+{
+    
+    if (1 > n) return 0;
+    if ((n % 2) != 0) n = n-1;
+    Console.Write(n + ", ");
+
+   return EvenNumberRec(n - 2);
+
+}
+
+
---
+++ 002/Program.cs
@@ -0,0 +1,36 @@
+﻿/*Задача 66: Задайте значения M и N.
+ Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30*/
+
+
+Console.WriteLine("Введите число M ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число N ");
+int n = int.Parse(Console.ReadLine());
+
+void SumElement(int N, int M)
+{
+    int sum = M;
+    while (N > M)
+    {
+        sum = sum + N; 
+        N--;
+    }
+    Console.Write(sum);
+}
+SumElement(n, m); 
+Console.WriteLine();
+Console.WriteLine("проверка");
+
+int SumElementRec(int M, int N)  //рекурсия
+{
+
+    if (N < M) return 0;
+    
+    else return N + SumElementRec(M, N - 1);
+    
+}
+
+Console.Write(SumElementRec(m, n));
